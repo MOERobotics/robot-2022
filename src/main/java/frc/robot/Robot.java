f0460ff031591f9c1476404e2f75e97eac464916
DeathDIVer <daniel.qian950@gmail.com>
2022-02-20 13:21:12 -0500

    updated robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -30,7 +30,7 @@ public class Robot extends TimedRobot {
   GenericRobot robot = new TurretBot();
   Joystick joystick = new Joystick(0);
   Joystick xbox = new Joystick(1);
-  GenericAutonomous autonomous = autoArc;
+  GenericAutonomous autonomous = simpleCTerminal;
 
 
   int averageTurretXSize = 2;
